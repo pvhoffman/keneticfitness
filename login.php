9cdd566519ab67abbbc802055a625065d2c3f230
Paul V. Hoffman <pvhoffman@gmail.com>
2015-03-18 16:44:45 -0700

    added code to login.php

--- login.php
+++ login.php
@@ -11,8 +11,24 @@ do{
         break;
     }
     // password and email are set, validate against the db
-    $json = json_encode(array("userid" => 222));
+    try {
+        $conn = new PDO ("sqlsrv:server = tcp:lxi9spfs47.database.windows.net,1433; Database = keneticfitness-database", "keneticfitness-database", "{J>a3B0;mp}");
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+        $stmt = $conn->prepare("select client_id from client_t where client_email = :email and client_password = :password ");
+        $stmt->bindParam(":email", $_GET["email"]);
+        $stmt->bindParam(":password", $_GET["password"]);
+        $stmt->execute();
+        $recs = $stmt->fetchAll();
+        if(!$recs){
+            $json = json_encode(array("errors" => "Invalid user name or password."));
+        }
+    } catch ( PDOException $e ) {
+        $json = json_encode(array("errors" => "Error connecting to SQL Server: " . $e->getMessage()));
+    }
 
 } while(false);
 echo $json
 ?>
+
+
